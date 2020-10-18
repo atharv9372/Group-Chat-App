41398d980115e1a46023323dff85666369b1f1eb
atharv9372 <73017258+atharv9372@users.noreply.github.com>
2020-10-18 17:35:45 +0530

    Create firebase.js

---
+++ Frontend/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+apiKey: "AIzaSyCwMuTiL-HiyqNuEINFXfWKMLyQQhGM",
+authDomain: "whatsapp-mern-1e3d5.firebaseapp.com",
+databaseURL: "https://whatsapp-mern-1e3d5.firebaseseio.com",
+projectId: "whatsapp-mern-1e3d5",
+storageBucket: "whatsapp-mern-1e3d5.appspot.com",
+messagingSenderId: "474278923489",
+appId: "474278923489.web: 5904d55785474033057ba0",
+};
